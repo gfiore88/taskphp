78971f894451ede7496179fffb05bdff52e34202
gfiore88 <info@giovannifiore.net>
2016-04-09 15:55:17 +0200

    - Refactoring of LanguageBatchBo class

---
+++ src/ApiErrors.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Language;
+
+/**
+ * Description of ApiErrors
+ *
+ * @author giovannifiore
+ */
+class ApiErrors {
+	/**
+	 * Checks the api call result.
+	 *
+	 * @param mixed  $result   The api call result to check.
+	 *
+	 * @throws Exception   If the api call was not successful.
+	 *
+	 * @return void
+	 */
+        public static function checkForApiErrorResult($result)
+	{
+		// Error during the api call.
+		if ($result === false || !isset($result['status'])) {
+			throw new \Exception('Error during the api call');
+		}
+		// Wrong response.
+		if ($result['status'] != 'OK') {
+			throw new \Exception('Wrong response: '
+				. (!empty($result['error_type']) ? 'Type(' . $result['error_type'] . ') ' : '')
+				. (!empty($result['error_code']) ? 'Code(' . $result['error_code'] . ') ' : '')
+				. ((string)$result['data']));
+		}
+		// Wrong content.
+		if ($result['data'] === false) {
+			throw new \Exception('Wrong content!');
+		}
+	}
+}
